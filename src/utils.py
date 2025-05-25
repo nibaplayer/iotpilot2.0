a36410a7d9b4797a7a96ae7189225d0afaf17371
hao <1127007806@qq.com>
2025-05-25 21:46:45 +0800

    extract_module_code support python3

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,12 @@ from pydantic import SecretStr
 from typing import Union
 from config import *
 import re
+import json
+import os
 def get_llm(model: str, temperature: float = 0)->Union[ChatOpenAI,None]:
     if model not in CANDIATE_MODEL:
         raise ValueError(f"Model {model} is not supported. Supported models are: {CANDIATE_MODEL}")
-    elif model in ["gpt-4o"]:
+    elif model in ["gpt-4o", "gpt-3.5-turbo", "gpt-4o-mini"]:
         llm = ChatOpenAI(model="gpt-4o", temperature=temperature, base_url=OPENAI_BASE_URL, api_key=SecretStr(OPENAI_KEY))
 
     return llm
@@ -22,6 +24,16 @@ def extract_module_code(text: str) -> list:
     Returns:
         List[str]: A list of strings representing the extracted code blocks.
     """
-    pattern = r"```[a-zA-Z]*\s*([\s\S]*?)\s*```"
+    pattern = r"```[a-zA-Z+]*\s*([\s\S]*?)\s*```"
     matches = re.findall(pattern, text, re.DOTALL)
     return matches
+
+def load_workflow_config(workflow_config_file_path: str) -> dict:
+    """Load workflow config from json file and return a list of WorkflowConfig namedtuples."""
+    if os.path.exists(workflow_config_file_path):
+        with open(workflow_config_file_path, "r", encoding="utf-8") as f:
+            workflow_config = json.load(f)
+        print("Workflow config loaded from file:", workflow_config_file_path)
+    else:
+        raise FileNotFoundError(f"配置文件 '{workflow_config_file_path}' 未找到！")
+    return workflow_config
